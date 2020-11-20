09fdd40498d8a8b790da3e8628905e4930f0c9f3
Tolga İskender <tolga@basefy.com>
2020-11-20 20:50:42 +0300

    Initial Commit

---
+++ YoutubeWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YoutubeWithSwiftUI
+//
+//  Created by Tolga İskender on 20.11.2020.
+//  Copyright © 2020 Tolga İskender. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
